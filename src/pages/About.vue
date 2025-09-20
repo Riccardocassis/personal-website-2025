cc5f3bb0fbec5fde6ef77a6f9f922b43f5f15f5a
Riccardo98 <riccardocassis.rc@gmail.com>
2025-09-20 04:04:31 +0200

    Ristrutturato il layout della pagina About: avvolto le immagini in div per una migliore gestione del layout e aggiunta di effetti hover al titolo.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -10,10 +10,14 @@
       </p>
     </div>
     <div class="w-full flex flex-col items-center justify-center mt-16">
-    <img src="../assets/ioartista.png" alt="Artista" class="fixed left-0 bottom-0 w-[38vw] min-w-[320px] max-w-none object-contain z-0 pointer-events-none" />
-    <img src="../assets/ionerd.png" alt="Nerd" class="fixed right-0 bottom-0 w-[38vw] min-w-[320px] max-w-none object-contain z-0 pointer-events-none" />
+    <div class="fixed left-0 bottom-0 w-[38vw] min-w-[320px] max-w-none z-0">
+      <img src="../assets/ioartista.png" alt="Artista" class="w-full object-contain opacity-70 transition duration-300" />
+    </div>
+    <div class="fixed right-0 bottom-0 w-[38vw] min-w-[320px] max-w-none z-0">
+      <img src="../assets/ionerd.png" alt="Nerd" class="w-full object-contain opacity-70 transition duration-300" />
+    </div>
         <div class="relative w-full h-[400px] mt-2 z-10">
-          <h2 class="absolute left-1/2 -translate-x-1/2 top-[80px] w-auto text-4xl md:text-5xl font-bold text-cyan-400 flex justify-center items-center tracking-tight drop-shadow-lg" style="position:absolute;">Il Giusto mix tra artista e nerd.</h2>
+    <h2 class="absolute left-1/2 -translate-x-1/2 top-[80px] w-auto text-4xl md:text-5xl font-bold text-cyan-400 flex justify-center items-center tracking-tight drop-shadow-lg transition duration-300 hover:text-transparent hover:[text-shadow:_0_0_16px_#22d3ee,0_2px_8px_#000] hover:[-webkit-text-stroke:1px_#22d3ee]" style="position:absolute;">Il Giusto mix tra artista e nerd.</h2>
         </div>
     </div>
   </section>
