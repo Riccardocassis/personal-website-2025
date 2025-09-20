75a4546593a817e939faf3713443a7f6e323f7cf
Riccardo98 <riccardocassis.rc@gmail.com>
2025-09-20 20:52:07 +0200

    Aggiunta pagina di caricamento: implementato un componente LoadingPage con animazioni e stili per migliorare l'esperienza utente durante il caricamento.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup>
 import Navbar from './components/navbar.vue'
 import Hero from './components/hero.vue'
+import { ref, onMounted } from 'vue'
+import LoadingPage from './components/LoadingPage.vue'
+
+const isLoading = ref(true)
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false
+  }, 3000)
+})
 </script>
 
 <template>
-  <div class="bg-black text-white min-h-screen overflow-hidden">
+  <LoadingPage v-if="isLoading" />
+  <div v-else class="bg-black text-white min-h-screen overflow-hidden">
     <Navbar />
     <router-view />
   </div>
